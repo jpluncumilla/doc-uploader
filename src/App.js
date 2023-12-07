b30840105fc9c3c6aab73be610592de0fe205256
Juan Pablo Luncumilla <jpluncumilla@Juans-MacBook-Pro-2.local>
2023-12-06 19:35:53 -0500

    Initial dialog button with state set true

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { useState } from 'react';
+import { Button } from '@mui/material';
 
 function App() {
+  const [openDialog, setOpenDialog] = useState(true)
+  
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <div className='App-header'>
+        <Button variant="outlined">
+          Open dialog
+        </Button>
+      </div>
     </div>
   );
 }
