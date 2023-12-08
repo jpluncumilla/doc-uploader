cc39724dc12e6ecc42d436a0eafac13ecb9d3f1f
Juan Pablo Luncumilla <jpluncumilla@Juans-MacBook-Pro-2.local>
2023-12-08 12:50:40 -0500

    Uploader submit function

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,20 +1,33 @@
 import { Button } from "@mui/material"
-import React from "react"
+import React, { useState } from "react"
 
 export const Uploader = () => {
-    const handleSubmit = () => {
+    const [file, setFile] = useState<File | undefined>()
 
+    const handleSubmit = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+
+        if (typeof file === 'undefined') return
+        const formData = new FormData()
+        formData.append('file', file)
+
+        //TODO: Add cloud storage for upload
+        console.log('STORED to state ====>', file)
     }
     
     const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
         const target = e.target as HTMLInputElement & { files: FileList }
+        //TODO remove console log
         console.log('files', target.files)
+
+        setFile(target.files[0])
+
     }
     return (
         <>
         <h1>Uploader</h1>
         <input type="file" name="manifest" onChange={handleOnChange}/>
-        <Button onSubmit={handleSubmit}>Upload Manifest</Button>
+        <Button onClick={handleSubmit}>Upload Manifest</Button>
         </>
     )
 }
