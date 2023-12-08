d6b2fd463f1cc9210d6ba175eef3c57653824c90
Juan Pablo Luncumilla <jpluncumilla@Juans-MacBook-Pro-2.local>
2023-12-08 13:38:36 -0500

    react dropzone package added and dropzone area is now styled

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,5 +1,7 @@
-import { Button } from "@mui/material"
-import React, { useState } from "react"
+import { Box, Button, Grid, Typography } from "@mui/material"
+import DescriptionIcon from '@mui/icons-material/Description';
+import React, { useState, useCallback } from "react"
+import {useDropzone} from 'react-dropzone'
 
 export const Uploader = () => {
     const [file, setFile] = useState<File | undefined>()
@@ -11,7 +13,7 @@ export const Uploader = () => {
         const formData = new FormData()
         formData.append('file', file)
 
-        //TODO: Add cloud storage for upload
+        //TODO: Add cloud storage for upload with axios POST request for loader
         console.log('STORED to state ====>', file)
     }
     
@@ -21,13 +23,41 @@ export const Uploader = () => {
         console.log('files', target.files)
 
         setFile(target.files[0])
-
     }
+    //Dropzone functionality
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+      }, [])
+    
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
     return (
         <>
-        <h1>Uploader</h1>
-        <input type="file" name="manifest" onChange={handleOnChange}/>
-        <Button onClick={handleSubmit}>Upload Manifest</Button>
+        <Box mt={2} sx={{border: 'solid 1px #a8a8a8', borderRadius:'10px',padding:'1em'}}>
+            <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            { !isDragActive && (
+                // @ts-ignore
+                <Grid item align='center' sx={{border:'dashed 1px #a8a8a8', p: '3em'}}>
+                    <DescriptionIcon sx={{color:'#cf6304'}}/>
+                    <Typography>Drag & Drop Here or <strong>Browse</strong></Typography>
+                </Grid>
+            )
+            }
+
+            { isDragActive && (
+                // @ts-ignore
+                <Grid item align='center' sx={{border:'dashed 1px #a8a8a8', p: '3em'}}>
+                    <Typography><strong>File will be uploaded</strong></Typography>
+                </Grid>
+            )
+            }
+            </div>
+            {// @ts-ignore
+            <Grid item align='center' mt={2}>
+            <Button size="medium" variant="contained" onClick={handleSubmit}>Upload Manifest</Button>
+            </Grid>  }      
+        </Box>
         </>
     )
 }
